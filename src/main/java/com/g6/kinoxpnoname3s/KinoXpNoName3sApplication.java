1a0426ff3bbb4c9534a75aa183d9270a84163ff6
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-10-01 10:59:43 +0200

    Initial commit

---
+++ src/main/java/com/g6/kinoxpnoname3s/KinoXpNoName3sApplication.java
@@ -0,0 +1,13 @@
+package com.g6.kinoxpnoname3s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpNoName3sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinoXpNoName3sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g6/kinoxpnoname3s/KinoXpNoName3sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g6.kinoxpnoname3s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpNoName3sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
